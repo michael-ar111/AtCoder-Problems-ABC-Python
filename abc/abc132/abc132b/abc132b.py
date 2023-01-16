9d08b0be7d0982f2ebb996ceacd31275bae239cc
michael-ar111 <michael111ar@gmail.com>
2023-01-16 05:08:48 +0000

    abc132b

---
+++ abc/abc132/abc132b/abc132b.py
@@ -0,0 +1,12 @@
+N = int(input())
+P = list(map(int,input().split()))
+
+ans = 0
+
+for i in range(N-2):
+    if P[i] < P[i+1] < P[i+2]:
+        ans += 1
+    elif P[i] > P[i+1] > P[i+2]:
+        ans += 1
+
+print(ans)
