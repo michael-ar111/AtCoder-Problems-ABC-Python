199c4e128417fd22e7c08b88bc3f243cd2e2e602
michael-ar111 <michael111ar@gmail.com>
2023-01-26 01:38:29 +0000

    past202004_c modify comment

--- past202004-open/past202004_c/past202004_c.py
+++ past202004-open/past202004_c/past202004_c.py
@@ -1,22 +1,14 @@
 N = int(input())
 #リストでないと実施不可だった。各要素[".","#"]とするため、listでないと[".#"]となる。
-S = [list("...") for _ in range(N)]
-
-ans = []
-
+S = [list(input()) for _ in range(N)]
 
 #N-2から引いた数から-1ずつ引いていく。-1まで、つまり0を実施して終了。
 for i in range(N-2, -1, -1):
-    #
     for j in range(2*N-1):
         if S[i][j] == "#":
-            #ここの理解が必要！
+            #i+1 一つ下の、j-1で左。i+1 一つ下の、jで真ん中。i+1 一つ下の、j+1で右。
             if S[i+1][j-1] == "X" or S[i+1][j] == "X" or S[i+1][j+1] == "X":
                 S[i][j] = "X"
 
-#ここの理解が必要！
+#連想配列として渡して、一つの配列は一つjoinで一つの文字列。連想配列で2つ目に改行。
 for t in S: print("".join(t))
-
-# for i in range(N):
-#     ans[i] = "".join(S[i])
-#     print(ans[i])
