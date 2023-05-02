85269b685eade1cf6cd6f3ea8bf17328ef04b35c
michael-ar111 <michael111ar@gmail.com>
2023-05-02 06:30:56 +0000

    abc163c fix message

--- abc/abc163/abc163c/abc163c.py
+++ abc/abc163/abc163c/abc163c.py
@@ -5,7 +5,7 @@ a = list(map(int, input().split()))
 G = [[] for i in range(N)]
 
 for i in range(1, N):
-    #aのリストを参照し、1なら0とし、appendは適当な値を追加する。
+    #aのリストを参照し、1なら0とし、appendは適当な値を追加する。aにはプラスの値が入っているので、そこからインデックスの値で参照する。
     G[a[i - 1] - 1].append(i - 1)
 
 for i in G:
