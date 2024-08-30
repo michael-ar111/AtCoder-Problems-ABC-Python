ea440a48c63dcd22fbc70d0d0bd6c85f50e627cb
michael-ar111 <michael111ar@gmail.com>
2024-08-30 08:26:36 +0000

    abc061b_20240830

--- abc/abc061/abc061b/abc061b.py
+++ abc/abc061/abc061b/abc061b.py
@@ -1,13 +1,29 @@
-n, m = map(int, input().split())
-# グラフの実装
-# 連想配列
-G = [[] for i in range(n)]
-for i in range(m):
-    u,v = map(int, input().split())
-    u -= 1
-    v -= 1
-    # u,vの位置の配列に値をいれる。appendの値は何でもいい。最終的な個数を出力するため。
-    G[u].append(v)
-    G[v].append(u)
-for i in range(n):
-    print(len(G[i]))
+# n, m = map(int, input().split())
+# # グラフの実装
+# # 連想配列
+# G = [[] for i in range(n)]
+# for i in range(m):
+#     u,v = map(int, input().split())
+#     u -= 1
+#     v -= 1
+#     # u,vの位置の配列に値をいれる。appendの値は何でもいい。最終的な個数を出力するため。
+#     G[u].append(v)
+#     G[v].append(u)
+# for i in range(n):
+#     print(len(G[i]))
+
+#20240830:復習
+N,M = map(int, input().split())
+#各都市間の道路初期化
+road_count = [0] * N
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    #a都市間の道路数加算
+    road_count[a - 1] += 1
+    #b都市間の道路数加算
+    road_count[b - 1] += 1
+
+#各都市の道路数
+for count in road_count:
+    print(count)
