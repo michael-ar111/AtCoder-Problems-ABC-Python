cfbb41eb8cf8e31fd2a6c8f19758228533a57334
michael-ar111 <michael111ar@gmail.com>
2024-08-30 08:27:31 +0000

    abc163c_20240830

--- abc/abc163/abc163c/abc163c.py
+++ abc/abc163/abc163c/abc163c.py
@@ -1,12 +1,27 @@
+# N = int(input())
+
+# a = list(map(int, input().split()))
+# #N個リストを作成する
+# G = [[] for i in range(N)]
+
+# for i in range(1, N):
+#     #aのリストを参照し、1なら0とし、appendは適当な値を追加する。aにはプラスの値が入っているので、そこからインデックスの値で参照する。
+#     G[a[i - 1] - 1].append(i - 1)
+
+# for i in G:
+#     print(len(i))
+
+#20240830:復習
 N = int(input())
+A = list(map(int, input().split()))
 
-a = list(map(int, input().split()))
-#N個リストを作成する
-G = [[] for i in range(N)]
+subordinates = [0] * N
 
-for i in range(1, N):
-    #aのリストを参照し、1なら0とし、appendは適当な値を追加する。aにはプラスの値が入っているので、そこからインデックスの値で参照する。
-    G[a[i - 1] - 1].append(i - 1)
+#各社員分まわす
+for i in A:
+    #各社員の一つ上の上司に部下の数を加算
+    subordinates[i-1] += 1
 
-for i in G:
-    print(len(i))
+#各社員の部下の数を出力
+for count in subordinates:
+    print(count)
